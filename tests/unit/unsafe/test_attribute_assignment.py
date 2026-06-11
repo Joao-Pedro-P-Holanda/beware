9d844e5c83e4cdb2cc8c203bd6cea65a1f0de617
João Pedro <joaopedroph.dev@gmail.com>
2026-06-11 19:55:19 -0300

    Correct condition for python < 3.12 checking
    Previous behavior failed for python 3.11.x

--- tests/unit/unsafe/test_attribute_assignment.py
+++ tests/unit/unsafe/test_attribute_assignment.py
@@ -12,7 +12,7 @@ class Example:
 def test_unsafe_descriptor_cant_be_assigned_to_two_attributes():
     # On python < 3.12 the __set_name__ call wraps exceptions with RuntimeError
     # https://github.com/python/cpython/pull/103402
-    exception = NameAssignmentException if sys.version_info > (3, 11) else RuntimeError
+    exception = RuntimeError if sys.version_info < (3, 12) else NameAssignmentException
     with pytest.raises(exception):
         shared_descriptor = unsafe()
 
