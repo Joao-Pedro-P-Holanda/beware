d64cb73428d8f458a13fdb2c99425c025baa93e6
João Pedro <joaopedroph.dev@gmail.com>
2026-06-11 19:53:19 -0300

    Expect RuntimeError instead of NameAssignmentException on descriptor reassignment for python 3.11 and below

--- tests/unit/unsafe/test_attribute_assignment.py
+++ tests/unit/unsafe/test_attribute_assignment.py
@@ -1,4 +1,5 @@
 import pytest
+import sys
 from beware import unsafe
 from beware.exceptions import NameAssignmentException
 
@@ -9,7 +10,10 @@ class Example:
 
 
 def test_unsafe_descriptor_cant_be_assigned_to_two_attributes():
-    with pytest.raises(NameAssignmentException):
+    # On python < 3.12 the __set_name__ call wraps exceptions with RuntimeError
+    # https://github.com/python/cpython/pull/103402
+    exception = NameAssignmentException if sys.version_info > (3, 11) else RuntimeError
+    with pytest.raises(exception):
         shared_descriptor = unsafe()
 
         class ReuseDescriptor:
